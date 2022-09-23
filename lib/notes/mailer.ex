909cd51434dc460a377c7b6e595b62fc066c597f
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-09-22 23:49:18 -0300

    ci: :green_heart: config CI

--- lib/notes/mailer.ex
+++ lib/notes/mailer.ex
@@ -1,3 +1,4 @@
 defmodule Notes.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :notes
 end
--- lib/notes_web/telemetry.ex
+++ lib/notes_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule NotesWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
